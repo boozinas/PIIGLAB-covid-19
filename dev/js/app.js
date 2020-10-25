7493574ad31080009a1be570fb69a22f17fc2314
Boozinas <boozmedina@gmail.com>
2020-10-25 00:37:35 -0500

    Enlazando el servidor de archivos con la peticion realizada con Angular para la secciones

--- dev/js/app.js
+++ dev/js/app.js
@@ -2,27 +2,48 @@
  'use strict';
  angular.module('Maps',[])
  .controller('SlideController', SlideController)
+ .controller('CategoryMapsController', CategoryMapsController)
  .service('MapListService', MapListService);
 
 SlideController.$inject = ['MapListService'];
 function SlideController(MapListService){
     var slide = this;
     slide.condition = false;
-    slide.slides = [];
-    slide.getSlide = function(keyword){
-        slide.slides = MapListService.checkKeywordFolder(keyword);
-        console.log(slide.slides);
-    };
+    var promise = MapListService.checkKeywordFolder();
+
+    promise.then(function (response) {
+        slide.slides = response.data;
+    })
+    .catch(function(error){
+        console.log("Something went wrong");
+    });
+
+    // slide.getSlide = function(keyword){
+    //     slide.slides = MapListService.checkKeywordFolder(keyword);
+    //     console.log(slide.slides);
+    // };
 }
 
-function MapListService(){
+CategoryMapsController.$inject = ['MapListService'];
+function CategoryMapsController(MapListService){
+    var category = this;
+
+}
+
+MapListService.$inject = ['$http']
+function MapListService($http){
     var service = this;
     var mapList = []; //Para guardar la lista de direcciones de las imagenes
     var keywordFolder = ""; //Para almacenar la palabra clave de la carpeta donde estan las imagenes a presentar
-    //
+    
     // Hace la peticion al Backend mandando la clave del directorio para devolver un arreglo con imagenes
-    service.checkKeywordFolder = function (keyword){
-        return ['1.png', '2.png', '3.png', keyword]
+    service.checkKeywordFolder = function (){
+        var response = $http({
+            method: "GET",
+            url: ("http://127.0.0.1:3001/url")
+        });
+        console.log(response);
+        return response;
     };
 }
     
--- dev/js/loadFiles.js
+++ dev/js/loadFiles.js
@@ -1,11 +1,27 @@
+var express = require("express");
+const fs = require('fs');
+var app = express();
+const rootFolder = './public/assets/img';
 
-// const testFolder = '../../public/assets/img';
-// const fs = require('fs');
-// const list = []
-// fs.readdir(testFolder, (err, files) => {
-//   files.forEach(file => {
-//     list.push(file)
-//     console.log(file);
-//   });
-//   console.log(list);
-// });
+app.get("/url", (req, res, next) => {
+    const list = [];
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type'); // If needed
+    res.setHeader('Access-Control-Allow-Credentials', true); // If needed;
+    var innerFolder = rootFolder + '';
+    res.json(getImgs(list, innerFolder));
+});
+
+function getImgs(list, folder) {
+    fs.readdirSync(folder).forEach(file => {
+        list.push(file);
+      });
+    console.log(list);
+   return list; 
+}
+   
+app.listen(3001,() => {
+    console.log("Server runnning on port 3001");
+});
+
