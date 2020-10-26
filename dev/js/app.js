8998bb759dcd893e067c407da4c443edefe304a2
Boozinas <boozmedina@gmail.com>
2020-10-25 21:31:03 -0600

    Escaneo de categorias e imagenes correcto

--- dev/js/app.js
+++ dev/js/app.js
@@ -3,20 +3,36 @@
  angular.module('Maps',[])
  .controller('SlideController', SlideController)
  .controller('CategoryMapsController', CategoryMapsController)
- .service('MapListService', MapListService);
+ .service('MapListService', MapListService)
+ .constant('ApiBasePath', "http://127.0.0.1:3001/url");
 
 SlideController.$inject = ['MapListService'];
 function SlideController(MapListService){
     var slide = this;
     slide.condition = false;
-    var promise = MapListService.checkKeywordFolder();
+    //Obtencion de categorias en la carpeta global
+    var promise = MapListService.getCategoriesCOVID19();
 
     promise.then(function (response) {
-        slide.slides = response.data;
+        slide.categories = response.data;
     })
     .catch(function(error){
         console.log("Something went wrong");
     });
+    //obtencion de archivos (imagenes) de la carpeta particular
+
+    slide.getImagesByCategory = function(category) {
+        slide.condition = true;
+       var promise = MapListService.getImages(category);
+
+       promise.then(function (response){
+           slide.slides = response.data;
+           console.log(response.data);
+       })
+       .catch(function (error){
+           console.log(error);
+       })
+    };
 
     // slide.getSlide = function(keyword){
     //     slide.slides = MapListService.checkKeywordFolder(keyword);
@@ -30,19 +46,32 @@ function CategoryMapsController(MapListService){
 
 }
 
-MapListService.$inject = ['$http']
-function MapListService($http){
+MapListService.$inject = ['$http', 'ApiBasePath']
+function MapListService($http, ApiBasePath){
     var service = this;
     var mapList = []; //Para guardar la lista de direcciones de las imagenes
     var keywordFolder = ""; //Para almacenar la palabra clave de la carpeta donde estan las imagenes a presentar
     
     // Hace la peticion al Backend mandando la clave del directorio para devolver un arreglo con imagenes
-    service.checkKeywordFolder = function (){
+    service.getCategoriesCOVID19 = function (){
         var response = $http({
             method: "GET",
-            url: ("http://127.0.0.1:3001/url")
+            url: (ApiBasePath)
         });
         console.log(response);
+        return response;
+    };
+
+    service.getImages = function (categoryName){
+        console.log(categoryName);
+        var response = $http({
+            method: "GET",
+            url: (ApiBasePath),
+            params: {
+                category: categoryName
+            }
+        });
+
         return response;
     };
 }
--- dev/js/loadFiles.js
+++ dev/js/loadFiles.js
@@ -1,7 +1,9 @@
 var express = require("express");
 const fs = require('fs');
 var app = express();
-const rootFolder = './public/assets/img';
+const rootFolder = './public/assets/img/';
+const relativeFolder = 'assets/img/';
+var categoryFolder = '';
 
 app.get("/url", (req, res, next) => {
     const list = [];
@@ -9,13 +11,26 @@ app.get("/url", (req, res, next) => {
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type'); // If needed
     res.setHeader('Access-Control-Allow-Credentials', true); // If needed;
-    var innerFolder = rootFolder + '';
-    res.json(getImgs(list, innerFolder));
+    var innerFolder = rootFolder;
+    if(req.query.category){
+        categoryFolder = req.query.category;
+        innerFolder = innerFolder +  categoryFolder;
+        res.json(getImgs(list, innerFolder));
+    }else{
+        res.json(getFolder(list, innerFolder));
+    }
 });
 
-function getImgs(list, folder) {
+function getFolder(list, folder) {
     fs.readdirSync(folder).forEach(file => {
         list.push(file);
+      });
+    console.log(list);
+   return list; 
+}
+function getImgs(list, folder) {
+    fs.readdirSync(folder).forEach(file => {
+        list.push(relativeFolder+categoryFolder+"/"+file);
       });
     console.log(list);
    return list; 
